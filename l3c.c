561d947b5523257983063944074ce9cd0015d11d
Oscar M Alvarez G <104049116+miusarname@users.noreply.github.com>
2024-05-14 10:41:21 -0500

    feat: :sparkles: Records, condition flags and opcodes are created, and I declare that I am working in 128 kb.

---
+++ l3c.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+/* unix only */
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+#define MEMORY_MAX (1 << 16)
+
+uint16_t memory[MEMORY_MAX]; 
+
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,
+    R_COND,
+    R_COUNT
+};
+
+uint16_t reg[R_COUNT];
+
+enum 
+{
+    OP_BR=0,
+    OP_ADD,
+    OP_LD,
+    OP_ST,
+    OP_JSR,
+    OP_AND,
+    OP_LDR,
+    OP_STR,
+    OP_RTI,
+    OP_NOT,
+    OP_LDI,
+    OP_STI,
+    OP_JMP,
+    OP_RES,
+    OP_LEA,
+    OP_TRAP
+};
+
+enum
+{
+    FL_POS = 1 << 0,
+    FL_ZRO = 1 << 1,
+    FL_NEG = 1 << 2
+};
